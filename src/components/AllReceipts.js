e78dded69fdda1e4e2265293cbc66a7325a60464
Jonah Alexander-Awad <jonahawad94@live.com>
2019-08-20 14:34:27 -0400

    Added Receipt-History
    Added receipt history to user home page, displays all receipts, needs to filter based on userId

---
+++ src/components/AllReceipts.js
@@ -0,0 +1,65 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import axios from 'axios';
+
+
+
+class AllReceipts extends Component{
+    state={
+        receipts: []
+    }
+
+    componentDidMount(){
+        axios.get('https://vpay-heroku.herokuapp.com/api/receipts')
+            .then(res => {
+                console.log(res)
+                this.setState ({
+                    receipts: res.data
+                })
+
+            })
+    }
+    render(){
+        const {receipts} = this.state;
+        const receiptList = receipts.length ? (
+            receipts.map(receipt => {
+                return (
+                    <div className="receipt" key={receipt.id}>
+                        <div className="receiptContent">
+                            <span className="receiptId">Receipt No: {receipt.id}</span>
+                            <div className="totalPrice">Total:
+                                <div>{receipt.totalPrice}</div>
+                            </div>
+                            <div className="tip">Tip:
+                                <div>{receipt.tipPercent}</div>
+                            </div>
+
+                        </div>
+                    </div>
+                )
+            })
+        ) : (
+            <div className="noReceipts"> No receipts yet</div>
+        )
+
+        return (
+            <div className='receiptHistory'>
+                <div className="receiptHistoryTitle"> RECEIPT HISTORY
+                    <div classNam="orders">
+                        {receiptList}
+                    </div>
+                <div className="totalPrice"> TOTAL PRICE:
+                </div>
+            </div>
+        </div>
+        );
+    }
+}
+
+    // const orders= axios.get("https://vpay-heroku.herokuapp.com/api/orders").then(res => {
+    // this.setState({orders: res.data});
+    // });
+
+    // }
+
+export default AllReceipts;
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 import './HomePage.css';
+import AllReceipts from './AllReceipts'
+
 
 class HomePage extends Component {
     constructor(props) {
@@ -20,7 +22,7 @@ class HomePage extends Component {
       })
       console.log(event.target.files[0])
   
-  }
+    }
 
     render() {
     return (
@@ -30,21 +32,26 @@ class HomePage extends Component {
         
         <div className="container">
             <div className="row">
-            <div className="col-md-6">
+              <div className="col-md-6">
                 <form method="post" action="#" id="#">
-                <div className="form-group files">
-                  <label>Upload Your File </label>
-                  <input type="file" className="form-control" name="file"  onChange={this.onChangeHandler} multiple=""></input>
-                </div>
-              </form>
+                  <div className="form-group files">
+                    <label>Upload Your File </label>
+                    <input type="file" className="form-control" name="file"  onChange={this.onChangeHandler} multiple=""></input>
+                  </div>
+                </form>
               <div><button type="button" className="uploadButton" onClick={()=>this.props.fetchReceiptData(this.state.selectedFile)}>Upload</button>{this.props.data.amounts ?
             <Link  to="/orders"><button className="toOrdersButtonS" >Proceed</button></Link>
             : <button className="toOrdersButtonB">No Orders</button>}</div>
             </div>
+
+            <div className="receiptHistory">
+              <AllReceipts/>
+            </div>
+
+            </div>
+
           </div>
         </div>
-
-    </div>
     )
   }
   }
