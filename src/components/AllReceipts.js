b8ee1da124312c489c290ef7a640eaedfd510937
thomas-westfall <thomas.westfall64@myhunter.cuny.edu>
2019-08-21 16:33:02 -0400

    merged but showorders doesnt work

--- src/components/AllReceipts.js
+++ src/components/AllReceipts.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 import './AllReceipts.css';
 import loggeduser from '../store/utilities/loggeduser';
 
 class AllReceipts extends Component {
     state = {
-        receipts: []
+        receipts: [],
+        orders: [],
+        view: true
     }
 
     componentDidMount() {
@@ -13,9 +16,17 @@ class AllReceipts extends Component {
             .then(res => {
                 console.log(res)
                 this.setState({
-                    receipts: res.data
+                    receipts: res.data,
                 })
             })
+            let allOrders = [];
+            this.state.receipts.map(receipt => {
+                allOrders.push(receipt.orders)
+            })
+            this.setState({
+                orders: allOrders
+            })
+        
     }
 
     render() {
@@ -40,11 +51,36 @@ class AllReceipts extends Component {
                 <div className="noReceipts"> No receipts yet</div>
             )
 
+            const orderList = () => {
+                let allOrders = [];
+                this.state.orders.map(order => {
+                    order.map(item => {
+                        allOrders.push(
+                            <div className="order" key={item.id}>
+                                <div className="orderContent">
+                                    <p>
+                                        <span className="orderId"><u>Order No:</u> {item.id}</span>
+                                        <div className="orderName"><u>Order Name:</u> {item.itemName}</div>
+                                        <div className="orderPrice"><u>Price:</u> {item.price}</div>
+                                    </p>
+                                </div>
+                            </div>
+                        )
+                    }
+    
+                    )
+                    allOrders.push(<div>---------------------</div>)
+                })
+                console.log(allOrders);
+                return allOrders;
+            }
+    
         return (
             <div className='receiptHistory'>
                 <div className="receiptHistoryTitle"> RECEIPT HISTORY
-                    <div className="orders">
-                        {receiptList}
+<div className="orders">
+                    {this.state.view ? receiptList : orderList()}
+                    <button type="button" className="showOrderButton" onClick={() => this.setState({ view: !this.state.view })}>Show Orders</button>
                     </div>
                 </div>
             </div>
