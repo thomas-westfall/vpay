96d3099a51bb0136735d7605d1e9a3d562c8b2bb
Jonah Alexander-Awad <34581478+JonahAwad94@users.noreply.github.com>
2019-08-21 15:14:27 -0400

    Added Receipt View

--- src/components/AllReceipts.js
+++ src/components/AllReceipts.js
@@ -1,27 +1,36 @@
-import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 import './AllReceipts.css';
 import loggeduser from '../store/utilities/loggeduser';
 
 
-class AllReceipts extends Component{
-    state={
-        receipts: []
+class AllReceipts extends Component {
+    state = {
+        receipts: [],
+        orders: [],
+        view: true
     }
 
-    componentDidMount(){
-        axios.get('https://vpay-heroku.herokuapp.com/api/receipts' + '/' + this.props.loggeduser.id)
+    async componentDidMount() {
+        await axios.get('https://vpay-heroku.herokuapp.com/api/receipts' + '/' + this.props.loggeduser.id)
             .then(res => {
-                console.log(res)
-                this.setState ({
+                this.setState({
                     receipts: res.data
                 })
             })
+        let allOrders = [];
+        this.state.receipts.map(receipt => {
+            allOrders.push(receipt.orders)
+        })
+        this.setState({
+            orders: allOrders
+        })
     }
 
-    render(){
-        const {receipts} = this.state;
+
+    render() {
+        const { receipts } = this.state;
         const receiptList = receipts.length ? (
             receipts.map(receipt => {
                 return (
@@ -29,26 +38,49 @@ class AllReceipts extends Component{
                         <div className="receiptContent">
                             <p>
                                 <span className="receiptId"><u>Receipt No:</u> {receipt.id}</span>
-                                <div className="totalPrice"><u>Total:</u> {receipt.totalPrice}
-                                </div>
-                                <div className="tip"><u>Tip:</u> {receipt.tipPercent}
-                                </div>
+                                <div className="totalPrice"><u>Total:</u> {receipt.totalPrice}</div>
+                                <div className="tip"><u>Tip:</u> {receipt.tipPercent}</div>
                             </p>
                         </div>
                     </div>
                 )
             })
         ) : (
-            <div className="noReceipts"> No receipts yet</div>
-        )
+                <div className="noReceipts"> No receipts yet</div>
+            )
+
+        const orderList = () => {
+            let allOrders = [];
+            this.state.orders.map(order => {
+                order.map(item => {
+                    allOrders.push(
+                        <div className="order" key={item.id}>
+                            <div className="orderContent">
+                                <p>
+                                    <span className="orderId"><u>Order No:</u> {item.id}</span>
+                                    <div className="orderName"><u>Order Name:</u> {item.itemName}</div>
+                                    <div className="orderPrice"><u>Price:</u> {item.price}</div>
+                                </p>
+                            </div>
+                        </div>
+                    )
+                }
+
+                )
+                allOrders.push(<div>---------------------</div>)
+            })
+            console.log(allOrders);
+            return allOrders;
+        }
 
         return (
             <div className='receiptHistory'>
                 <div className="receiptHistoryTitle">
                     Receipt History
                 </div>
-                <div classNam="orders">
-                    {receiptList}
+                <div className="orders">
+                    {this.state.view ? receiptList : orderList()}
+                    <button type="button" className="showOrderButton" onClick={() => this.setState({ view: !this.state.view })}>Show Orders</button>
                 </div>
             </div>
         );
