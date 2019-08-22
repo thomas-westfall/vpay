46a16b2970350bd4e9fa76dbb2f7e7529cb34d50
NapatKarm <napat.karm@gmail.com>
2019-08-21 20:47:44 -0400

    sign up progress

--- src/components/RegisterPage.js
+++ src/components/RegisterPage.js
@@ -61,13 +61,13 @@ class Register extends Component {
               <tr><td colSpan={2}><h1 className="registerTitle">Sign Up</h1></td></tr>
             </thead>
             <tbody >
-              <tr><td className="TextField">Username:</td><td><input type="text" className="Username" onChange={this.handleChangeUsername} /></td></tr>
-              <tr><td className="TextField">Password:</td><td><input type="password" className="Password" id="password" onChange={this.handleChangePassword}></input></td></tr>
+              <tr><td className="TextField">Username:</td><td className="inputFieldR"><input type="text" className="Username" onChange={this.handleChangeUsername} /></td></tr>
+              <tr><td className="TextField">Password:</td><td className="inputFieldR"><input type="password" className="Password" id="password" onChange={this.handleChangePassword}></input></td></tr>
               <tr><td colSpan={2} className="showPassword"><input type="checkbox" className="showPassCheck"id='show-password' onClick={this.showPass}></input><label for='show-password'>Show Password</label></td></tr>
-              <tr><td className="TextField">First Name:</td><td><input type="text" className="firstName" onChange={this.handleChangeFirstName} /></td></tr>
-              <tr><td className="TextField">Last Name:</td><td><input type="text" className="lastName"  onChange={this.handleChangeLastName}></input></td></tr>
-              <tr><td className="TextField">Phone Number:</td><td><input type="number" className="phoneNumber" onChange={this.handleChangePhone} /></td></tr>
-              <tr><td className="TextField">Email:</td><td><input type="text" className="email"  onChange={this.handleChangeEmail}></input></td></tr>
+              <tr><td className="TextField">First Name:</td><td className="inputFieldR"><input type="text" className="firstName" onChange={this.handleChangeFirstName} /></td></tr>
+              <tr><td className="TextField">Last Name:</td><td className="inputFieldR"><input type="text" className="lastName"  onChange={this.handleChangeLastName}></input></td></tr>
+              <tr><td className="TextField">Phone Number:</td><td className="inputFieldR"><input type="number" className="phoneNumber" onChange={this.handleChangePhone} /></td></tr>
+              <tr><td className="TextField">Email:</td><td className="inputFieldR"><input type="text" className="email"  onChange={this.handleChangeEmail}></input></td></tr>
               <tr>{this.props.registerSuccess ? <td colSpan={2} className="successMessage">{this.props.registerSuccess}</td> : ""}{this.props.registerError ? <td colSpan={2} className ="errorMessage">{this.props.registerError.data}</td> : ""}</tr>
               <tr><td colSpan={2}><button className="registerButton">Register</button></td></tr>
               <tr><td colSpan={2}><Link className="cancelButton"to ="/">Cancel</Link></td></tr>
