b6e05ef7d81daeec47e672c369d928a33a7e46b0
thomas-westfall <thomas.westfall64@myhunter.cuny.edu>
2019-08-22 00:12:49 -0400

    show orders works, but no button

--- src/components/AllReceipts.js
+++ src/components/AllReceipts.js
@@ -8,7 +8,7 @@ class AllReceipts extends Component {
     state = {
         receipts: [],
         orders: [],
-        view: true
+        view: false
     }
 
     componentDidMount() {
@@ -26,14 +26,17 @@ class AllReceipts extends Component {
             this.setState({
                 orders: allOrders
             })
-        
     }
+
 
     render() {
         const { receipts } = this.state;
+        var allOrders = [];
         const receiptList = receipts.length ? (
+            
             receipts.map(receipt => {
                 return (
+                    <td>
                     <div className="receipt" key={receipt.id}>
                         <div className="receiptContent">
                             <span className="receiptId">Receipt No: {receipt.id}</span>
@@ -43,44 +46,52 @@ class AllReceipts extends Component {
                             </div>
                             <div className="tip"><u>Tip:</u> {receipt.tipPercent}
                             </div>
+                            <div className="orders"><u>Orders:</u>
+                            {allOrders = []}
+                            {receipt.orders.map(item => {
+                            allOrders.push(
+                                <div id={item.id} className="order" key={item.id}>
+                                    <div className="orderContent">
+                                        <p>
+                                            <span className="orderId"><u>Order No:</u> {item.id}</span>
+                                            <div className="orderName"><u>Order Name:</u> {item.itemName}</div>
+                                            <div className="orderPrice"><u>Price:</u> {item.price}</div>
+                                        </p>
+                                    </div>
+                                </div>
+                            )
+                        
+        
+                        
+                            })
+                        }
+                        {console.log(allOrders.length)}
+
+                        {/* for the showhide button. doesnt work (showhides all receipts orders) */}
+                        {/* {this.state.view ? (
+                            <div>
+                            <button type="button" className="showOrderButton" onClick={() => this.setState({ view: !this.state.view })}>Hide Orders</button>
+                            {allOrders}
+                            </div>
+            
+                            ): (<button type="button" className="showOrderButton" onClick={() => this.setState({ view: !this.state.view })}>Show Orders</button>
+                        )} */}
+                         {allOrders}
+                            </div>
                         </div>
                     </div>
+                    </td>
                 )
             })
         ) : (
                 <div className="noReceipts"> No receipts yet</div>
             )
 
-            const orderList = () => {
-                let allOrders = [];
-                this.state.orders.map(order => {
-                    order.map(item => {
-                        allOrders.push(
-                            <div className="order" key={item.id}>
-                                <div className="orderContent">
-                                    <p>
-                                        <span className="orderId"><u>Order No:</u> {item.id}</span>
-                                        <div className="orderName"><u>Order Name:</u> {item.itemName}</div>
-                                        <div className="orderPrice"><u>Price:</u> {item.price}</div>
-                                    </p>
-                                </div>
-                            </div>
-                        )
-                    }
-    
-                    )
-                    allOrders.push(<div>---------------------</div>)
-                })
-                console.log(allOrders);
-                return allOrders;
-            }
-    
         return (
             <div className='receiptHistory'>
                 <div className="receiptHistoryTitle"> RECEIPT HISTORY
-<div className="orders">
-                    {this.state.view ? receiptList : orderList()}
-                    <button type="button" className="showOrderButton" onClick={() => this.setState({ view: !this.state.view })}>Show Orders</button>
+                    <div className="orders">
+                    {receiptList}
                     </div>
                 </div>
             </div>
